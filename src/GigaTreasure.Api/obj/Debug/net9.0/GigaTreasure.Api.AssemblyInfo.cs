85b1cb6a74ef32dcf85dafe4504f9adb3976710d
danielclara13 <137109348+danielclara13@users.noreply.github.com>
2025-08-05 16:51:07 +0100

    Updated AssemblyInformationalVersion attributes across projects to include commit hash for versioning consistency.

--- src/GigaTreasure.Api/obj/Debug/net9.0/GigaTreasure.Api.AssemblyInfo.cs
+++ src/GigaTreasure.Api/obj/Debug/net9.0/GigaTreasure.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GigaTreasure.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5e545cb63f5d3dd73405e33d168cdc86146cafc")]
 [assembly: System.Reflection.AssemblyProductAttribute("GigaTreasure.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GigaTreasure.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/GigaTreasure.Infra.Ioc/obj/Debug/net9.0/GigaTreasure.Infra.Ioc.AssemblyInfo.cs
+++ src/GigaTreasure.Infra.Ioc/obj/Debug/net9.0/GigaTreasure.Infra.Ioc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GigaTreasure.Infra.Ioc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5e545cb63f5d3dd73405e33d168cdc86146cafc")]
 [assembly: System.Reflection.AssemblyProductAttribute("GigaTreasure.Infra.Ioc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GigaTreasure.Infra.Ioc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
